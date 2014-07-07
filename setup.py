400b7e71356d18aa54095cfc86d1d3208f475cbd
Russell Horton <rhorton@ecohealth.io>
2014-07-07 13:34:46 -0700

    use setuptools instead of disutils so we can use install_requires; ignore annie.egg files generate by installs

--- setup.py
+++ setup.py
@@ -1,5 +1,10 @@
-from distutils.core import setup
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
 setup(name='annie',
-	version='0.1',
+	version='0.1.1',
 	packages=['annotator', ],
+	install_requires=['lazy', ]
 )
