8af0fdd63cfb919e8e4e13f3dcb4d844cc239f92
Russell Horton <rhorton@ecohealth.io>
2014-07-31 11:09:04 -0700

    fix dupe test, fix jvm-nlp url

--- tests/annotator/test_jvm_nlp_annotator.py
+++ tests/annotator/test_jvm_nlp_annotator.py
@@ -5,7 +5,7 @@ webservice providing Stanford NLP results.
 As these tests require the presence of a well-running webservice, they may be
 considered more of an integration test than a unit test.
 
-The code for the webservice required is github.com/ecohealthalliance/nlp-jvm
+The code for the webservice required is github.com/ecohealthalliance/jvm-nlp
 """
 
 import sys
@@ -63,36 +63,6 @@ class JVMNLPAnnotatorTest(unittest.TestCase):
         self.assertEqual(doc.tiers['times'].spans[0].text, 'Tomorrow')
         self.assertEqual(doc.tiers['times'].spans[0].start, 0)
         self.assertEqual(doc.tiers['times'].spans[0].end, 8)
-
-    def test_relative_dates(self):
-
-        annotator = JVMNLPAnnotator(['times'])
-
-        text = "Tomorrow I'm going to the symphony. In five days from now I'll wish it was yesterday."
-        doc = AnnoDoc(text, date=datetime.datetime(2010, 10, 10))
-        doc.add_tier(annotator)
-
-        self.assertEqual(doc.text, text)
-
-        self.assertEqual(len(doc.tiers['times'].spans), 3)
-
-        self.assertEqual(doc.tiers['times'].spans[0].label, '2010-10-11')
-        self.assertEqual(doc.tiers['times'].spans[0].text, 'Tomorrow')
-        self.assertEqual(doc.tiers['times'].spans[0].start, 0)
-        self.assertEqual(doc.tiers['times'].spans[0].end, 8)
-        self.assertEqual(doc.tiers['times'].spans[0].type, 'DATE')
-
-        self.assertEqual(doc.tiers['times'].spans[1].label, '2010-10-15')
-        self.assertEqual(doc.tiers['times'].spans[1].text, 'five days from now')
-        self.assertEqual(doc.tiers['times'].spans[1].start, 39)
-        self.assertEqual(doc.tiers['times'].spans[1].end, 57)
-        self.assertEqual(doc.tiers['times'].spans[1].type, 'DATE')
-
-        self.assertEqual(doc.tiers['times'].spans[2].label, '2010-10-09')
-        self.assertEqual(doc.tiers['times'].spans[2].text, 'yesterday')
-        self.assertEqual(doc.tiers['times'].spans[2].start, 75)
-        self.assertEqual(doc.tiers['times'].spans[2].end, 84)
-        self.assertEqual(doc.tiers['times'].spans[2].type, 'DATE')
 
     def test_relative_dates(self):
 
