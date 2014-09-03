a9d087f31494df16133f58d5dd314e4482cb6500
Russell Horton <rhorton@ecohealth.io>
2014-09-03 15:58:12 -0700

    update shell script to use country info

--- import_geonames.sh
+++ import_geonames.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 wget http://download.geonames.org/export/dump/allCountries.zip
-unzip allCountries.zip
+wget http://download.geonames.org/export/dump/countryInfo.txt
+unzip allCountries.zip -d diagnosis
 rm allCountries.zip
-python mongo_import_geonames.py --mongo_url $MONGO_URL
+cd diagnosis && python mongo_import_country_info.py && python mongo_import_geonames.py
 rm allCountries.txt
+rm countryInfo.txt
+cd ..
