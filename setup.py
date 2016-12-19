3d23a172a1f94ea12bfa836cfe0b025f33faf6d4
Nathan Breit <nabreit@gmail.com>
2016-12-19 09:37:10 -0800

    bump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ except ImportError:
     from distutils.core import setup
 
 setup(name='annie',
-	version='0.2.1',
+	version='0.2.2',
 	packages=['annotator', ],
 	install_requires=['lazy', 'nltk', 'pattern', ]
 )
