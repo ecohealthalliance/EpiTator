72747029c62ccdd1766bcde857b68312ff220230
Nathan Breit <nath@nathanathan.com>
2017-02-13 05:41:32 -0500

    Add __version__ attribute

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
+import os
 try:
     from setuptools import setup
 except ImportError:
     from distutils.core import setup
 
+with open(os.path.join(os.path.dirname(__file__), 'annotator', 'version.py')) as f:
+    exec(f.read())
+
 setup(
     name='annie',
-	version='0.3.1',
+	version=__version__,
 	packages=['annotator', ],
 	install_requires=['lazy', 'nltk', 'pattern', 'numpy']
 )
+
