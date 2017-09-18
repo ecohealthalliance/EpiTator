1157e804b1e6b64adf6d62eee646dc62370822ce
Nathan Breit <nath@nathanathan.com>
2017-09-18 08:56:29 -0400

    fix py3 doctest bug

--- run_doctests.py
+++ run_doctests.py
@@ -1,8 +1,18 @@
+from __future__ import print_function
+import doctest
 if __name__ == "__main__":
-    import doctest
-    import epitator.annotier
-    doctest.testmod(epitator.annotier)
-    import epitator.annospan
-    doctest.testmod(epitator.annospan)
-    import epitator.annodoc
-    doctest.testmod(epitator.annodoc)
+    raise_on_error = True
+    try:
+        import epitator.annotier
+        doctest.testmod(epitator.annotier, raise_on_error=raise_on_error)
+        import epitator.annospan
+        doctest.testmod(epitator.annospan, raise_on_error=raise_on_error)
+        import epitator.annodoc
+        doctest.testmod(epitator.annodoc, raise_on_error=raise_on_error)
+    except (doctest.UnexpectedException, doctest.DocTestFailure) as e:
+        print("Failed example:")
+        print(e.example.lineno, ":", e.example.source)
+        print("Expected:", e.example.want)
+        print("Got:", e.got)
+        import sys
+        sys.exit(1)
